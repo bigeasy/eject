b13312315fe7bebd4cd14d26bf6a49eba2b623c1
Alan Gutierrez <alan@prettyrobots.com>
2016-06-08 20:57:35 -0400

    Implement ejectable Cadence interface.

--- t/eject.t.js
+++ t/eject.t.js
@@ -1,13 +1,32 @@
-require('proof')(1, prove)
+require('proof')(4, prove)
 
 function prove (assert) {
-    var eject = require('..')
+    var ejectable = require('..')
 
-    eject()
-
-    try {
-        eject(new Error('ejected'))
-    } catch (error) {
-        assert(error.message, 'ejected', 'thrown')
+    function echo (value, callback) {
+        callback(null, value)
     }
+
+    var f = ejectable(function (async, value) {
+        assert(value, 1, 'parameters')
+        async([function () {
+            async.eject()
+            async()(null, 1)
+        }, function (error) {
+            assert(error.message, 'ejected', 'eject')
+        }], function () {
+            async()(null, 1)
+        }, [function (value) {
+            assert(value, 1, 'continuing')
+            var wait = async()
+            async.eject()
+            wait()
+        }, function (error) {
+            assert(error.message, 'ejected', 'eject outstanding')
+        }])
+    })
+
+    f(1, function (error) {
+        if (error) throw error
+    })
 }
